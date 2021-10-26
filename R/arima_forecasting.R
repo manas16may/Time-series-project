f9a5b605ef64f55b6cdffff1b941bcc3f62b2241
Arkajyoti Bhattacharjee <arkastat98@gmail.com>
2021-10-26 23:42:47 +0530

    minor update

--- R/arima_forecasting.R
+++ R/arima_forecasting.R
@@ -1,33 +1,33 @@
 library(tseries)
 library(forecast)
-rm(list=ls())
-data=read.csv('data.csv')
-data$Date <- as.Date(data$Date)
-data1=read.csv('testdata.csv')
-data1$Date=as.Date(data1$Date)
+rm(list = ls()) # remove existing environmental variables
+data = read.csv('data/train_data.csv') 
+data$Date <- as.Date(data$Date) 
+data1 = read.csv('data/test_data.csv')
+data1$Date = as.Date(data1$Date)
 head(data1)
 head(data)
 str(data)
-final=rbind(data,data1)
+final = rbind(data, data1)
 tail(final)
 str(final)
-price=final$Close
-log_return=diff(log(price),lag=1)
-final=final[-1,]
+price = final$Close
+log_return = diff(log(price), lag=1)
+final = final[-1, ]
 head(final)
-final['return']=log_return
-plot(final$return,type="o")
+final['return'] = log_return
+plot(final$return, type="o")
 tail(final)
-y_train<- final[1:221, ]
+y_train<- final[1:245, ]
 tail(y_train)
-y_test<-final[222:431,]
+y_test<-final[246:431,]
 tail(y_test)
 ## Checking Stationary of log data (y)
 adf.test(final$return, alternative = "stationary")
 ## ACF and PACF of the error
 par(mfrow=c(1,2))
-Acf(y_train$return, lag.max = 40, main='ACF')
-Pacf(y_train$return, lag.max = 40, main = 'PACF')
+Acf(y_train$return, lag.max = 100, main='ACF')
+Pacf(y_train$return, lag.max = 100, main = 'PACF')
 # AR value to 35 is sufficient
 ## Fitting ARIMA Model
 best.order = c(0, 0, 0)
@@ -43,7 +43,7 @@ for (q in 1:33) for (p in 1:35) {
 }
 best.order; best.aic
 model.fit = best.arima
-tsdisplay(residuals(model.fit), lag.max = 40,
+tsdisplay(residuals(model.fit), lag.max = 10,
 main ="ARIMA (11,0,2) Model Residuals")
 checkresiduals(model.fit, lag.max = 40)
 library(astsa)
@@ -55,3 +55,6 @@ p1 = forecast1$pred
 obs1 = y_test$return
 MSE_foreast1 = mean((p1 - obs1)^2)
 MSE_foreast1
+
+## using auto.arima
+auto.arima(y_train$return,max.p=100,max.q=100,max.d=100)
